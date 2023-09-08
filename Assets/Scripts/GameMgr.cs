160eab3410b595d1e67d98ceee90b4e19390c259
Timothy Ngo <77961406+Timothy-Ngo@users.noreply.github.com>
2023-09-07 18:37:01 -0700

    Spawnpoints
    Need to fix how entities are spawned by the spawn points

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    public static GameMgr inst;
+    private void Awake()
+    {
+        inst = this;
+    }
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = Vector3.zero;
+        foreach(GameObject go in EntityMgr.inst.entityPrefabs) {
+            Entity381 ent = EntityMgr.inst.CreateEntity(go.GetComponent<Entity381>().entityType, position, Vector3.zero);
+            position.x += 200;
+        }
+    }
+
+    public Vector3 position;
+    public float spread = 20;
+    public float colNum = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F12)) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    Entity381 ent = EntityMgr.inst.CreateEntity(Entity.EntityType.PilotVessel, position, Vector3.zero);
+                    position.z += spread;
+                }
+                position.x += spread;
+                position.z = 0;
+            }
+            DistanceMgr.inst.Initialize();
+        }
+    }
+}
---
+++ Assets/Scripts/NPVesselController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entity;
+using System;
+using System.Linq;
+using static UnityStandardAssets.Utility.TimedObjectActivator;
+
+public class NPVesselController : MonoBehaviour
+{
+    public Entity.EntityType entityType;
+    public List<Transform> transforms;
+    public Entity381 entity;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        entity = EntityMgr.inst.CreateEntity(entityType, transform.position, Vector3.zero);
+        MoveToCheckpoint(transforms[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void MoveToCheckpoint(Vector3 newPosition)
+    {
+        Debug.Log(entity);
+        Move m = new Move(entity, newPosition);
+        UnitAI ai = entity.GetComponent<UnitAI>();
+        ai.SetCommand(m);
+    }
+
+    public Entity381 SpawnNPVessel(Vector3 spawnPosition, Vector3 spawnEulerAngles)
+    {
+        Entity381 ent = null;
+        GameObject entityPrefab = EntityMgr.inst.entityPrefabs.Find(x => (x.GetComponent<Entity381>().entityType == entityType));
+        if (entityPrefab != null)
+        {
+           // GameObject entityGO = Instantiate(entityPrefab, spawnPosition, Quaternion.Euler(spawnEulerAngles), entitiesRoot.transform);
+        }
+
+        return ent;
+    }
+
+    /*
+    public Entity381 CreateEntity(EntityType et, Vector3 position, Vector3 eulerAngles)
+    {
+        Entity381 entity = null;
+        GameObject entityPrefab = entityPrefabs.Find(x => (x.GetComponent<Entity381>().entityType == et));
+        if (entityPrefab != null)
+        {
+            GameObject entityGo = Instantiate(entityPrefab, position, Quaternion.Euler(eulerAngles), entitiesRoot.transform);
+            if (entityGo != null)
+            {
+                entity = entityGo.GetComponent<Entity381>();
+                entityGo.name = et.ToString() + entityId++;
+                entities.Add(entity);
+            }
+        }
+        return entity;
+    }
+    */
+
+}
